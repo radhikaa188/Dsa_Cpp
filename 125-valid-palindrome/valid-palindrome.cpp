ad556dabd211d8220d6eb3d499b77b0d79b9d676
radhikaa188 <radhikaag18@gmail.com>
2025-12-20 14:07:57 +0530

    Time: 2 ms (40.15%) | Memory: 9.8 MB (93.63%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left=0;
+        int right=s.size()-1;
+        while(left<right){
+            //while s[left] is not alphanumeric then left ptr++
+            while(left<right && !isalnum(s[left])) left++;
+            // while s[right] is not alphanumeric then right ptr++
+            while(left<right && !isalnum(s[right])) right--;
+            // if not equal return false
+            if(tolower(s[left])!= tolower(s[right])) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
